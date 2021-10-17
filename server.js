816c6633bbe6c04a32ab90c584d6e5997ee22ccd
aviv-raztkovski <12847141+aviv-raztkovski@users.noreply.github.com>
2021-10-17 17:30:56 +0300

    added an id field to db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { port } = require('./config.json');
+const db = require('./DummyData')
 
 const app = express();
 
@@ -7,5 +8,7 @@ app.get('/example', (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.json({ message: 'Hi, from the server' })
 });
+
+console.log(db);
 
 app.listen(port, () => console.log(`server is listening on: ${port}`));
