b9b28c50050018fbcb529c5a3d8bff1626a2ddee
aviv-raztkovski <12847141+aviv-raztkovski@users.noreply.github.com>
2021-10-18 20:40:44 +0300

    create entrie finished with validetion

---
+++ utils/crud.js
@@ -0,0 +1,31 @@
+const db = require('../DummyData');
+
+const createEntries = (req, res, next) => {
+    const { isFeatured, listingTitle, listedCompany, listingLocation, timeRegistered, listingType, role, level, languages } = req.body.data;
+
+    if (!(isFeatured && listingTitle && listedCompany && listingLocation && timeRegistered && listingType && role && level && languages)) {
+        res.status(400).send({"message": "all fields must be provided"})
+    }
+    else {
+        const newEntrie = {
+            isFeatured: isFeatured,
+            listingTitle: listingTitle,
+            listedCompany: listedCompany,
+            listingLocation: listingLocation,
+            listingType: listingType,
+            timeRegistered: timeRegistered,
+            role: role,
+            level: level,
+            languages: languages
+        }
+        const id = db[db.length - 1].id + 1;
+        newEntrie.id = id;
+        db.push(newEntrie);
+
+        res.status(200).json(db);
+    }
+
+    return next();
+}
+
+module.exports = { createEntries };
