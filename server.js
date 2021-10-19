72bee621c9d3ab8397efe8d13bf6273d58cbdf46
aviv-raztkovski <12847141+aviv-raztkovski@users.noreply.github.com>
2021-10-19 22:00:30 +0300

    Added delete

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bp = require('body-parser');
 const { port } = require('./config.json');
-const { createEntries, readEntries, updateEntries } = require('./utils/crud');
+const { createEntries, readEntries, updateEntries, deleteEntries } = require('./utils/crud');
 
 const app = express();
 
@@ -18,5 +18,7 @@ app.post('/jobs', createEntries, (req, res) => {})
 app.get('/jobs', readEntries, (req, res) => {})
 
 app.put('/jobs/:id', updateEntries, (req, res) => {})
+
+app.delete('/jobs/:id', deleteEntries, (req, res) => {})
 
 app.listen(port, () => console.log(`server is listening on: ${port}`));
